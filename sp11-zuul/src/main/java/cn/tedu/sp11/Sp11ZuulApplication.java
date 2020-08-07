c92f6d5402d99bb8e016e1a7ca29bf4994829674
luojizhen <2821210387@qq.com>
2020-08-07 18:28:03 +0800

    luojizhen update All bootstrap.yml

--- sp11-zuul/src/main/java/cn/tedu/sp11/Sp11ZuulApplication.java
+++ sp11-zuul/src/main/java/cn/tedu/sp11/Sp11ZuulApplication.java
@@ -1,5 +1,6 @@
 package cn.tedu.sp11;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
